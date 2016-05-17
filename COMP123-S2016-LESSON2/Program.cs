a03f5bc84125384f37dda96ff66ac1eb3db116a4
abhinavsharda <abhinavsharda33@gmail.com>
2016-05-17 13:54:09 -0400

    Added a refrences to driver class from unit test

--- COMP123-S2016-LESSON2/Program.cs
+++ COMP123-S2016-LESSON2/Program.cs
@@ -4,12 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * Author: Abhinav
+ * Date: May 17, 2016
+ * Description: Adcanced demo lessons for lesson 2
+ * version: 0.0.2 -Added a refrences to driver class
+ *                 from unit test
+ */
+
+
 namespace COMP123_S2016_LESSON2
 {
-    class Program
+    /**
+     * Main Driver class For lesson 2
+     * 
+     * @class program
+     */
+    public class Program
     {
+        /**
+         *  Main method for our drive class
+         *  
+         * @constructor Main
+         * @param{string[]} args
+         */
         static void Main(string[] args)
         {
+            int x = 50;
+            int y = 40;
+            int result = 0;
+
+            result = addXandY(x, y);
+            Console.WriteLine(result);
+        }
+        static int addXandY(int firstNumber, int secondNumber)
+        {
+            int result = 0;
+            result = firstNumber + secondNumber;
+            return result;
         }
     }
 }
--- UnitTestforlesson2/UnitTest1.cs
+++ UnitTestforlesson2/UnitTest1.cs
@@ -1,14 +1,20 @@
-﻿using System;
+﻿
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace UnitTestforlesson2
+namespace UnitTest_FOR_LESSON_2
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTest_FOR_LESSON_2
     {
         [TestMethod]
         public void TestMethod1()
         {
+            //Arrange
+
+            //Act
+
+            // Assert
         }
     }
 }
